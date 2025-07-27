477460ad2f04824dd77ead403fff46be952339c3
Robert Garrett <robertgarrett404@gmail.com>
2025-07-27 10:41:00 -0500

    Fix: Mark the main-menu camera so it despawns
    The camera spawned for the main menu wasn't given the TitleUI marker
    component, so it wouldn't despawn when changing to the game scene.

--- src/title_screen.rs
+++ src/title_screen.rs
@@ -18,7 +18,7 @@ impl Plugin for GameMenuPlugin {
 struct TitleUI;
 
 fn spawn_menu(mut commands: Commands) {
-    commands.spawn(Camera2d);
+    commands.spawn((TitleUI, Camera2d));
     commands
         .spawn((
             TitleUI,
